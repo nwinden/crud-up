52b69e9e79068b148183229f5c6c576c4f29cee4
sionowen <sionowen34@gmail.com>
2016-05-19 10:35:48 -0500

    preparing to merge

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -1,3 +1,5 @@
+var data = [];
+
 $(function() {
 
   addToDropdown();
@@ -47,11 +49,19 @@ function getPets() {
     type: 'GET',
     url: '/pets',
     success: function (pets) {
-      console.log(pets);
-      pets.forEach(function (pets) {
-        $('td').append('<p>' + pets.first_name +
-        '</p>');
-      });
-    },
-  });
+      data = pets;
+      console.log(data[0]);
+      appendPet(0);
+    }
+  })
+}
+
+
+
+function appendPet (registeredOwner) {
+  //console.log('this ran', data[registeredOwner].first_name);
+   var targetData = data[registeredOwner]
+  $('.target-container').append('<tr><div><td>' + targetData.first_name + ' ' + targetData.last_name + '</td><td>' + targetData.pet_name + '</td><td>' + targetData.breed + '</td><td>' + targetData.color + '</td> <td> <button class="update"> Go </button> </td><td> <button class="delete"> Go </button> </td><td> <button class="checked_in"> IN </button> </td>  </tr>' )
+
+
 }
