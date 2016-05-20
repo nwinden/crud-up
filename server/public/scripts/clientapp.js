93e9cd64b58474c33713c29386d21b9d4cb35c66
sionowen <sionowen34@gmail.com>
2016-05-19 19:04:11 -0500

    finished checkin/out button and functionality

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -2,148 +2,186 @@ var data = [];
 
 $(function() {
 
-  addToDropdown();
+	addToDropdown();
 
 
-  $('.update').on('click', updatePet);
-  $('.registerOwner').on('click', registerOwner);
-  getPets();
+	$('.update').on('click', updatePet);
+	$('.registerOwner').on('click', registerOwner);
+	getPets();
 
-  $('.table').on('click', '.delete', function(){
+	$('.table').on('click', '.delete', function() {
 
-    deletePet($(this).parent().parent().find('.arrayIndex').text());
-    console.log('something', $(this).parent().parent().find('.arrayIndex').text());
-  });
+		deletePet($(this).parent().parent().find('.arrayIndex').text());
 
-  $('.addPet').on('click', function(){
-    addPet();
+	});
 
-  });
+	$('.addPet').on('click', function() {
 
-  $('.table').on('click', '.update', function(){
-    var updateInfo = {};
-    updateInfo['id'] = data[$(this).parent().parent().find('.arrayIndex').text()].id - 1;
-    updateInfo['pet_name'] = $(this).parent().parent().find('.pet_name').val();
-    updateInfo['breed'] = $(this).parent().parent().find('.breed').val();
-    updateInfo['color'] = $(this).parent().parent().find('.pet_color').val();
-    console.log('if update info is cool we win', updateInfo);
-    updatePet(updateInfo);
-  })
+		addPet();
+	});
+
+	$('.table').on('click', '.check_in', function() {
+    var checkIn = {};
+
+    checkIn['check_in_date'] = Date();
+    checkIn['pet_id'] = data[$(this).parent().parent().find('.arrayIndex').text() - 1].id;
+
+    $.ajax({
+      type: 'POST',
+      url: '/pets/check_in',
+      data: checkIn,
+      success: function(){
+
+      }
+
+    })
+		$(this).replaceWith('<button class="check_out">OUT</button>');
+
+	});
+
+
+	$('.table').on('click', '.check_out', function() {
+    var checkOut = {};
+
+    checkOut['check_out_date'] = Date();
+    checkOut['pet_id'] = data[$(this).parent().parent().find('.arrayIndex').text() - 1].id;
+
+    $.ajax({
+      type: 'PUT',
+      url:'/pets/check_out',
+      data: checkOut,
+      success: function(){
+
+      }
+
+    })
+    $(this).replaceWith('<button class="check_in">IN</button>');
+	});
+
+	$('.table').on('click', '.update', function() {
+		var updateInfo = {};
+		updateInfo['id'] = data[$(this).parent().parent().find('.arrayIndex').text()].id - 1;
+		updateInfo['pet_name'] = $(this).parent().parent().find('.pet_name').val();
+		updateInfo['breed'] = $(this).parent().parent().find('.breed').val();
+		updateInfo['color'] = $(this).parent().parent().find('.pet_color').val();
+		console.log('if update info is cool we win', updateInfo);
+		updatePet(updateInfo);
+	})
 
 
 });
 
-function updatePet (pet) {
-  event.preventDefault();
+function updatePet(pet) {
+	event.preventDefault();
 
-  $.ajax({
-    type: 'PUT',
-    url:'/pets',
-    data: pet,
-    success: function(){
+	$.ajax({
+		type: 'PUT',
+		url: '/pets',
+		data: pet,
+		success: function() {
 
-      getPets();
-    }
-  });
+			getPets();
+		}
+	});
 };
 
 
 function registerOwner() {
 
-  event.preventDefault();
+	event.preventDefault();
 
-  var owner = {};
+	var owner = {};
 
-  $.each($('.owner-registration').serializeArray(), function (i,field) {
+	$.each($('.owner-registration').serializeArray(), function(i, field) {
 
-    owner[field.name] = field.value;
+		owner[field.name] = field.value;
 
-  });
+	});
 
-  $.post('/owners', owner, function(response) {
+	$.post('/owners', owner, function(response) {
 
-    addToDropdown();
+		addToDropdown();
 
-  });
+	});
 
 }
 
 function addToDropdown() {
 
-  $.get('/owners', function(owners) {
+	$.get('/owners', function(owners) {
 
-    console.log(owners);
-    $('.owner_id').empty();
+		console.log(owners);
+		$('.owner_id').empty();
 
-    owners.forEach(function(owner) {
-      $('.owner_id').append('<option value="' + owner.id + '" class="selecter ' + owner.first_name + '">' + owner.first_name + ' ' + owner.last_name + '</option>');
-      $('.' + owner.first_name).data('ownerID', owner.id);
-    });
+		owners.forEach(function(owner) {
+			$('.owner_id').append('<option value="' + owner.id + '" class="selecter ' + owner.first_name + '">' + owner.first_name + ' ' + owner.last_name + '</option>');
+			$('.' + owner.first_name).data('ownerID', owner.id);
+		});
 
-  });
+	});
 
 
 }
 
 function deletePet(target) {
-  $.ajax({
-    type: 'DELETE',
-    url:'/pets',
-    data: data[target -1],
-    success: function(){
-      console.log('didnt fail');
-      getPets();
-    }
-  });
+	$.ajax({
+		type: 'DELETE',
+		url: '/pets',
+		data: data[target - 1],
+		success: function() {
+			console.log('didnt fail');
+			getPets();
+		}
+	});
 }
 
 function getPets() {
-  $.ajax({
-    type: 'GET',
-    url: '/pets',
-    success: function (pets) {
-      data = pets;
-      $('.target-container').empty();
-      for(var i = 0; i < data.length; ++i){
-        appendPet(i);
-      }
-    }
-  })
+	$.ajax({
+		type: 'GET',
+		url: '/pets',
+		success: function(pets) {
+			data = pets;
+			$('.target-container').empty();
+			for (var i = 0; i < data.length; ++i) {
+				appendPet(i);
+			}
+		}
+	})
 }
 
 
 
 function addPet() {
 
-  event.preventDefault();
+	event.preventDefault();
 
-  var pet = {};
+	var pet = {};
 
-  $.each($('.pet-registration').serializeArray(), function (i,field) {
-    pet[field.name] = field.value;
-  });
+	$.each($('.pet-registration').serializeArray(), function(i, field) {
+		pet[field.name] = field.value;
+	});
 
 
-  $.ajax({
-    type: 'POST',
-    url: '/pets',
-    data: pet,
-    success:function(success){
-      console.log('you didnt fuck up!');
-      getPets();
-    }
+	$.ajax({
+		type: 'POST',
+		url: '/pets',
+		data: pet,
+		success: function(success) {
+			console.log('you didnt fuck up!');
+			getPets();
+		}
 
-  });
+	});
 
 
 
 }
 
-function appendPet (registeredOwner) {
-  //console.log('this ran', data[registeredOwner].first_name);
-  console.log(data);
-   var targetData = data[registeredOwner]
-  $('.target-container').append('<tr><td class="arrayIndex">' + (registeredOwner + 1)+ '</td><td>' + targetData.first_name + ' ' + targetData.last_name + '</td><td><input class="pet_name" name="pet_name" type="text" value="' + targetData.pet_name + '"></input></td><td><input class="breed" name="breed" type="text" value="' + targetData.breed + '"></input></td><td><input class="pet_color" name="color" type="text" value="' + targetData.color + '"></input></td> <td> <button class="update"> Go </button> </td><td> <button class="delete"> Go </button> </td><td> <button class="check_in"> IN </button> </td>  </tr>' )
+function appendPet(registeredOwner) {
+	//console.log('this ran', data[registeredOwner].first_name);
+	console.log(data);
+	var targetData = data[registeredOwner]
+	$('.target-container').append('<tr><td class="arrayIndex">' + (registeredOwner + 1) + '</td><td>' + targetData.first_name + ' ' + targetData.last_name + '</td><td><input class="pet_name" name="pet_name" type="text" value="' + targetData.pet_name + '"></input></td><td><input class="breed" name="breed" type="text" value="' + targetData.breed + '"></input></td><td><input class="pet_color" name="color" type="text" value="' + targetData.color + '"></input></td> <td> <button class="update"> Go </button> </td><td> <button class="delete"> Go </button> </td><td> <button class="check_in"> IN </button> </td>  </tr>')
 
 
 }
--- server/routes/pets.js
+++ server/routes/pets.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 var pg = require('pg');
 var connectionString = 'postgres://localhost:5432/mu';
 
@@ -12,7 +13,7 @@ router.get('/', function (req, res) {
     client.query('SELECT pets.owner_id,pets.pet_name,pets.breed,pets.color,pets.id,owners.first_name,owners.last_name FROM pets JOIN owners ON owners.id = pets.owner_id', function (err, result) {
       done();
 
-      console.log(result.rows);
+
 
       res.send(result.rows);
     });
@@ -42,14 +43,35 @@ router.post('/', function (req, res) {
   });
 });
 
+router.post('/check_in/', function(req, res){
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+    console.log(req.body)
+    client.query('INSERT INTO visits (check_in_date, pet_id)' + 'values($1, $2)', [req.body.check_in_date, req.body.pet_id],
+    function(err, result) {
+      done();
+
+      if (err) {
+        res.sendStatus(500);
+      }
+
+      res.sendStatus(201);
+    });
+
+  });
+});
+
+
 router.delete('/', function (req, res) {
-  console.log('delete function', req.body);
+
   pg.connect(connectionString, function (err, client, done) {
     if (err) {
       res.sendStatus(500);
     }
 
-    client.query('DELETE FROM pets WHERE id =' + req.body.id,
+    client.query('DELETE FROM visits WHERE pet_id =' + req.body.id + '; DELETE FROM pets WHERE id =' + req.body.id,
       function(err, result){
         done();
         if (err) {
@@ -64,12 +86,10 @@ router.delete('/', function (req, res) {
 });
 
 router.put('/', function (req, res) {
-  console.log('updatePet function', req.body);
   pg.connect(connectionString, function (err, client, done) {
     if (err) {
       res.sendStatus(500);
     }
-
     client.query('UPDATE pets SET pet_name = $1 ,  breed = $2 , color = $3  WHERE id =' + req.body.id,
       [req.body.pet_name, req.body.breed, req.body.color],
       function(err, result){
@@ -82,9 +102,27 @@ router.put('/', function (req, res) {
         res.sendStatus(200);
       })
   });
-
 });
 
+router.put('/check_out/', function (req, res) {
+  console.log('check_out ran', req.body);
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+    client.query('UPDATE visits SET check_out_date = $1 WHERE pet_id =' + req.body.pet_id,
+      [req.body.check_out_date],
+      function(err, result){
+        done();
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+          return;
+        }
+        res.sendStatus(200);
+      })
+  });
+});
 
 
 
